889bbb2fe3455e9724c1dcd3769db906deb91bea
Alex Landherr <alex.landherr@gmail.com>
2023-09-27 19:22:43 +0000

    Added problem description as comment & the needed txt file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
+#include "include/functions.h"
 #include <iostream>
+
+/*
+The text file contains a 1000-digit number. The four adjacent digits in the 1000-digit number that have the greatest product are 9x9x8x9 = 5832.
+Find the thirteen (13) adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
+*/
 
 int main() {
 
